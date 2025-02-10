0cf645acd0fb4e04d8141dfe7deb270c53e86ad5
krekab16 <kunreka2001@gmail.com>
2025-02-10 21:32:18 +0100

    Face id business logic elkeszitese

--- application/lib/ui/face_id_screen_ui.dart
+++ application/lib/ui/face_id_screen_ui.dart
@@ -1,8 +1,12 @@
-import 'package:flutter/cupertino.dart';
-
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../utils/colors.dart';
+import '../utils/image_strings.dart';
+import '../utils/styles.dart';
+import '../utils/text_strings.dart';
+import '../viewmodel/faceid_view_model.dart';
 
 class FaceIdScreen extends StatefulWidget {
-
   const FaceIdScreen({Key? key}) : super(key: key);
 
   @override
@@ -13,7 +17,61 @@ class _FaceIdScreenState extends State<FaceIdScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Text("FACE");
+    final faceIdViewModel = Provider.of<FaceIdViewModel>(context);
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () => {Navigator.pop(context)},
+        ),
+        backgroundColor: MyColors.lightBlueColor,
+        title: Text(
+          logInWithFaceID,
+          style: Styles.textStyles,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: GestureDetector(
+                onTap: () async {
+                  await faceIdViewModel.authenticateUser();
+                  if (faceIdViewModel.isUserAuthorized) {
+                    faceIdViewModel.navigateToHome(context);
+                  } else {
+                    showDialog(
+                      context: context,
+                      builder: (_) => AlertDialog(
+                        title: Text(
+                          errorDialogTitle,
+                          style: Styles.errorText,
+                        ),
+                        content: Text(
+                          faceIdViewModel.errorMessages.join(" "),
+                          style: Styles.errorText,
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text(close),
+                          )
+                        ],
+                      ),
+                    );
+                  }
+                },
+                child: Image.asset(
+                  faceIdImage,
+                  height: 160,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
 }
